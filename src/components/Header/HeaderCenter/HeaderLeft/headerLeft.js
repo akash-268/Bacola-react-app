fe1ea7a0fcf33b4246e157955820290096f3eb9d
akash-268 <aakbajpai268@gmail.com>
2021-11-08 10:25:39 +0530

    some changes made

--- src/components/Header/HeaderCenter/HeaderLeft/headerLeft.js
+++ src/components/Header/HeaderCenter/HeaderLeft/headerLeft.js
@@ -7,46 +7,50 @@ import "../../../../css/header.css";
 import "../../../../css/main.css";
 
 import Sidebar from "./sidebar";
+import { Link } from "react-router-dom";
 
 const HeaderLeft = () => {
   const [clicked, setClicked] = useState(false);
   const sidebarHandler = () => {
-    setClicked(prev=>!prev);
+    setClicked((prev) => !prev);
   };
   return (
     <>
-    {clicked && <Sidebar/>}
-    <div className="column column-left">
-      <div className="header-buttons hide-desktop">
-        <div className="header-canvas button-item">
-          {" "}
-          <a href="#">
+      {clicked && <Sidebar />}
+      <div className="column column-left">
+        <div className="header-buttons hide-desktop">
+          <div className="header-canvas button-item">
             {" "}
-            <i className="klbth-icon-menu-thin" onClick={sidebarHandler}></i>{" "}
-          </a>{" "}
+            <a href="#">
+              {" "}
+              <i
+                className="klbth-icon-menu-thin"
+                onClick={sidebarHandler}
+              ></i>{" "}
+            </a>{" "}
+          </div>
+        </div>
+        <div className="site-brand">
+          <Link to="/" title="Bacola &#8211; Grocery Market and Food Theme">
+            {" "}
+            <img
+              className="desktop-logo hide-mobile"
+              src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/bacola-logo.png"
+              width="164"
+              height="44"
+              alt="Bacola &#8211; Grocery Market and Food Theme"
+            />
+            <img
+              className="mobile-logo hide-desktop"
+              src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/bacola-logo-mobile.png"
+              alt="Bacola &#8211; Grocery Market and Food Theme"
+            />{" "}
+            <span className="brand-description">
+              Online Grocery Shopping Center
+            </span>
+          </Link>
         </div>
       </div>
-      <div className="site-brand">
-        <a href="#" title="Bacola &#8211; Grocery Market and Food Theme">
-          {" "}
-          <img
-            className="desktop-logo hide-mobile"
-            src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/bacola-logo.png"
-            width="164"
-            height="44"
-            alt="Bacola &#8211; Grocery Market and Food Theme"
-          />
-          <img
-            className="mobile-logo hide-desktop"
-            src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/bacola-logo-mobile.png"
-            alt="Bacola &#8211; Grocery Market and Food Theme"
-          />{" "}
-          <span className="brand-description">
-            Online Grocery Shopping Center
-          </span>
-        </a>
-      </div>
-    </div>
     </>
   );
 };
